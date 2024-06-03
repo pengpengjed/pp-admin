731358798ecec79eb7f030a05f972a05e8dc0c5f
zhangyupeng <zhangyupeng@haotaisoft.com>
2024-06-03 11:31:25 +0800

    feat: 安装element-plus

--- internal/vite-config/src/plugins/index.ts
+++ internal/vite-config/src/plugins/index.ts
@@ -1,5 +1,8 @@
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 import { type PluginOption } from 'vite';
 import purgeIcons from 'vite-plugin-purge-icons';
 import DevTools from 'vite-plugin-vue-devtools';
@@ -55,6 +58,16 @@ async function createPlugins({ isBuild, root, enableMock, compress, enableAnalyz
   if (enableMock) {
     vitePlugins.push(configMockPlugin({ isBuild }));
   }
+
+  // Element-Plus
+  vitePlugins.push(
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  );
 
   return vitePlugins;
 }
